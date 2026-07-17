5f1750dd508cfd239d522f9bc1ec972cb9b41fd7
Wansun <wansunaryo@gmail.com>
2026-07-17 22:38:41 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/', function () {
+//     return redirect('/customers');
+// });
+
 
 Route::get('/', DashboardController::class)->name('dashboard');
-
-Route::get('/', function () {
-    return redirect('/customers');
-});
 
 Route::resource('customers', CustomerController::class);
