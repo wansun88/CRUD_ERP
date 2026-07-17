fbd2f898cc8a80e490b014aaa264821ad7468944
Wansun <wansunaryo@gmail.com>
2026-07-17 22:30:12 +0700

    dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<div class="app-content">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Dashboard</h3>
+                    </div>
+
+                    <div class="card-body">
+                        <h4>Selamat Datang di ERP Sales</h4>
+
+                        <p class="text-muted mb-0">
+                            Dashboard masih dalam tahap pengembangan.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
+
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', DashboardController::class)->name('dashboard');
 
 Route::get('/', function () {
     return redirect('/customers');
